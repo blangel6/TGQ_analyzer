108f529e1beb50dcc5d6b7c068768597348bc308
blangel6 <blangel6@gmail.com>
2026-09-20 09:39:49 +0900

    Track quality tablet app v1.0

---
+++ sw.js
@@ -0,0 +1,18 @@
+/* 오프라인 캐시: 처음 한 번 열어 두면 이후에는 인터넷 없이 실행된다 (HTTPS 또는 localhost 에서만 등록됨) */
+const CACHE = 'tqa-tablet-v1.0';
+const FILES = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(FILES)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET' || new URL(e.request.url).origin !== self.location.origin) return;
+  e.respondWith(
+    caches.match(e.request, { ignoreSearch: true }).then((hit) => hit || fetch(e.request).then((res) => {
+      const copy = res.clone(); caches.open(CACHE).then((c) => c.put(e.request, copy)); return res;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
